afdee3b3fd737a50a542038c6f15d9a967cbcb15
trail-fun <mik@bronze.ocn.ne.jp>
2026-09-15 22:46:51 +0900

    Install ESLint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config')
+const expoConfig = require("eslint-config-expo/flat")
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ["dist/*"]
+  },
+  {
+    rules: {
+      "semi": [2, "never"],
+      "comma-dangle": [2, "never"],
+      "@typescript-eslint/explicit-function-return-type": "error"
+    }
+  }
+])
