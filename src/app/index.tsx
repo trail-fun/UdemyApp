6ecb4f7d26c99759d94c48449bc7ed3aae57f879
trail-fun <mik@bronze.ocn.ne.jp>
2026-09-19 08:13:34 +0900

    Add Heder Style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,44 +4,51 @@ const Index = (): React.JSX.Element  => {
     return (
         <View style={styles.container}>
             <View>
-                <View>
-                    <View>
-                        <Text>Memo App</Text>
-                        <Text>ログアウト</Text>
-                    </View>
-                </View>
-
-                <View>
-                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2026年10月11日 11:39</Text>
-                    </View>
-                    <View>
-                        <Text>x</Text>
-                    </View>
-                </View>
-
-               <View>
-                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2026年10月11日 11:39</Text>
-                    </View>
-                    <View>
-                        <Text>x</Text>
-                    </View>
-                </View>
                 
-               <View>
-                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2026年10月11日 11:39</Text>
-                    </View>
-                    <View>
-                        <Text>x</Text>
+                <View style={styles.header}>
+                    <View style={styles.headerInner}>
+                        <Text style={styles.headerTitle}>Memo App</Text>
+                        <Text style={styles.headerRight}>ログアウト</Text>
                     </View>
                 </View>
-                
 
+                <View>
+
+                   <View>
+                        <View>
+                            <Text>買い物リスト</Text>
+                            <Text>2026年10月11日 11:39</Text>
+                        </View>
+                        <View>
+                            <Text>x</Text>
+                        </View>
+                    </View>
+
+                   <View>
+                        <View>
+                            <Text>買い物リスト</Text>
+                            <Text>2026年10月11日 11:39</Text>
+                        </View>
+                        <View>
+                            <Text>x</Text>
+                        </View>
+                    </View>
+
+                   <View>
+                        <View>
+                            <Text>買い物リスト</Text>
+                            <Text>2026年10月11日 11:39</Text>
+                        </View>
+                        <View>
+                            <Text>x</Text>
+                        </View>
+                    </View>
+
+
+
+
+                </View>
+ 
             </View>
 
             <View>
@@ -54,8 +61,28 @@ const Index = (): React.JSX.Element  => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },
+    header: {
+        backgroundColor: '#467fd4',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner: {
         alignItems: 'center'
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    headerRight: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255, 255, 255, 0.7)'
     }
 })
 
