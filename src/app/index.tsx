a9a634795fdfdddba6b274cfc663db978b6149ab
trail-fun <mik@bronze.ocn.ne.jp>
2026-09-19 06:16:23 +0900

    Markup memo list screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): React.JSX.Element  => { 
     return (
-        <View>
-            <Text>Hello, World!</Text>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <View>
+                        <Text>Memo App</Text>
+                        <Text>ログアウト</Text>
+                    </View>
+                </View>
+
+                <View>
+                     <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026年10月11日 11:39</Text>
+                    </View>
+                    <View>
+                        <Text>x</Text>
+                    </View>
+                </View>
+
+               <View>
+                     <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026年10月11日 11:39</Text>
+                    </View>
+                    <View>
+                        <Text>x</Text>
+                    </View>
+                </View>
+                
+               <View>
+                     <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026年10月11日 11:39</Text>
+                    </View>
+                    <View>
+                        <Text>x</Text>
+                    </View>
+                </View>
+                
+
+            </View>
+
+            <View>
+                <Text>+</Text>
+            </View>
+           
         </View>
     )  
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+
 
 export default Index
