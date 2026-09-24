4150767a49077842fd8d7c0ee6f6a4faf8a17a7a
trail-fun <mik@bronze.ocn.ne.jp>
2026-09-24 22:58:19 +0900

    Add memoListItems styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -14,45 +14,43 @@ const Index = (): React.JSX.Element  => {
 
                 <View>
 
-                   <View>
+                   <View style={styles.memoListItem}>
                         <View>
-                            <Text>買い物リスト</Text>
-                            <Text>2026年10月11日 11:39</Text>
+                            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                            <Text style={styles.memoListItemDate}>2026年10月11日 11:39</Text>
+                        </View>
+                        <View>
+                            <Text>x</Text>
+                        </View>
+                    </View>
+ 
+                  <View style={styles.memoListItem}>
+                        <View>
+                            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                            <Text style={styles.memoListItemDate}>2026年10月11日 11:39</Text>
                         </View>
                         <View>
                             <Text>x</Text>
                         </View>
                     </View>
 
-                   <View>
+                  <View style={styles.memoListItem}>
                         <View>
-                            <Text>買い物リスト</Text>
-                            <Text>2026年10月11日 11:39</Text>
+                            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                            <Text style={styles.memoListItemDate}>2026年10月11日 11:39</Text>
                         </View>
                         <View>
                             <Text>x</Text>
                         </View>
                     </View>
-
-                   <View>
-                        <View>
-                            <Text>買い物リスト</Text>
-                            <Text>2026年10月11日 11:39</Text>
-                        </View>
-                        <View>
-                            <Text>x</Text>
-                        </View>
-                    </View>
-
-
 
 
                 </View>
  
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
            
         </View>
@@ -83,6 +81,46 @@ const styles = StyleSheet.create({
         right: 16,
         bottom: 16,
         color: 'rgba(255, 255, 255, 0.7)'
+    },
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0, 0, 0, 0.15)'
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32
+    },
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
+    },
+    circleButton: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#467fd3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        shadowColor: '#000000',
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 8},
+        elevation: 8
+    },
+    circleButtonLabel: {
+        fontSize: 40,
+        lineHeight: 48,
+        color: '#ffffff'
     }
 })
 
